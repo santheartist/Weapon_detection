682879c182283030d1d2ea4f6a06ed7b1ea39991
Sanchit Panda <133005569+santheartist@users.noreply.github.com>
2025-06-27 22:03:04 +0530

    Add files via upload

---
+++ server side/alertupload_rest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path as url, include
+from rest_framework import routers
+from . import views
+from rest_framework.authtoken import views as rest_framework_views
+from django.urls import path, include
+
+urlpatterns = [
+    # Alert POST
+    path('images/', views.post_alert, name='post_alert'),
+     url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
+]
---
+++ server side/alertupload_rest/views.py
@@ -0,0 +1,16 @@
+from alertupload_rest.serializers import UploadAlertSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+
+@api_view(['POST'])
+def post_alert(request):
+    serializer = UploadAlertSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    else:
+        return JsonResponse(("Error: Unable to process data!"))
+
+    return Response(request.META.get('HTTP_AUTHORIZATION'))
